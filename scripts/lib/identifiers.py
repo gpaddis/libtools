8bd82428a8afa25392ad147b81a23708786672c3
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-02-05 11:59:46 +0100

    Refactor extract_isbns()

--- scripts/lib/identifiers.py
+++ scripts/lib/identifiers.py
@@ -1,12 +1,8 @@
 import isbnlib
 
 
-def extract_isbns(row):
-    "Extract all canonical isbns from a row in the CSV file."
-    all_isbns = []
-    for field in row:
-        isbns = isbnlib.get_isbnlike(field)
-        for isbn in isbns:
-            all_isbns.append(isbnlib.canonical(isbn))
-
-    return list(set(all_isbns))  # Deduplicate the ISBNs
+def extract_isbns(fields_list):
+    "Extract all canonical isbns from a list of fields."
+    all_isbns = [isbnlib.get_isbnlike(field) for field in fields_list]
+    canonical_isbns = [isbnlib.canonical(str(isbn)) for isbn in all_isbns if isbn != []]
+    return list(set(canonical_isbns))  # Deduplicate the ISBNs
--- scripts/unittests.py
+++ scripts/unittests.py
@@ -1,6 +1,28 @@
 import unittest
 import re
 import countmedia
+import lib.identifiers as ids
+
+class TestLibIdentifiers(unittest.TestCase):
+    """
+    Test the identifiers library methods.
+    """
+    def setUp(self):
+        self.row = [
+            "(R)Evolution",
+            "978-0-387-26125-6",
+            "978-0-387-26159-1",
+            "978-0-387-26159-1",
+            "http://link.springer.com/10.1007/b136864",
+            "Dekkers",
+            "10.1007/b136864",
+        ]
+
+    def test_extract_isbns(self):
+        self.assertEqual(
+            sorted(["9780387261591", "9780387261256"]),
+            sorted(ids.extract_isbns(self.row))
+        )
 
 class TestCountMedia(unittest.TestCase):
     """
